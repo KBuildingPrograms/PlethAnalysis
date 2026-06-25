b92ce001a3b6e88ca80105b00b68b18519dfb55f
KBuildingPrograms <kyd.00583@gmail.com>
2026-06-25 14:48:39 -0500

    - Function debugging

--- PA_Class_Func.py
+++ PA_Class_Func.py
@@ -80,37 +80,42 @@ def find_sighs(peak_dataframe,peak_height_mean,peak_area_mean): #i could add the
 def postsigh_apnea(normalized_signal, sigh):
     #iterating through sighs
     extended_view = normalized_signal.loc[(sigh.start_time < normalized_signal['Time']) & (normalized_signal['Time'] < sigh.start_time + 10), ['Time','Flow']] #extending 10 seconds out from sigh
-    extended_peaks = peak_analysis(extended_view, extended_view['Time'].iloc[0])
-    next_sigh = find_sighs(extended_peaks,peak_means(extended_view))
-    i=0
-    apneas = []
-    if next_sigh[0] is not None:
-        extended_peak_view = extended_peaks.loc[next_sigh[0].start_time > extended_view['Time']] 
-        while i < len(extended_peak_view) - 1:
+    extended_peaks = peak_analysis(extended_view, extended_view['Time'].iloc[0]) #gets the peaks of that ten second interval
+    extended_peak_hmean, extended_peak_amean = peak_means(extended_peaks)
+    next_sigh = find_sighs(extended_peaks,extended_peak_hmean, extended_peak_amean) #if there's another sigh within ten seconds, catch it
+    i=0 #indexing value for while loops
+    apneas = [] #to acquire apneas
+    if len(next_sigh) > 0: #if there is a sigh
+        extended_peak_view = extended_peaks.loc[next_sigh[0].start_time > extended_view['Time']] #condense the extended peak view to end before the next sigh
+        while i < len(extended_peak_view) - 2: #While the index is less than the maximum
             if extended_peak_view["Start"].iloc[i+1] - (extended_peak_view["Start"].iloc[i]+extended_peak_view["Width"].iloc[i]) > 0.7999:
+                #if the distance between the start of the next peak and the end of the first peak is greater than 0.7999, that means there's an apnea
                 apnea = Apnea("1/2", extended_peak_view["Start"].iloc[i] + extended_peak_view["Width"].iloc[i],[extended_peak_view["Start"].iloc[i+1] - (extended_peak_view["Start"].iloc[i] + extended_peak_view["Width"].iloc[i])])
-                apneas.append(apnea)
-            i+=1
+                #the apnea is defined as type 1 or 2, starting at the end of the first peak, and has a duration from the end of the last peak to the start of the next, placed in a list so more duration can be added if needed
+                apneas.append(apnea) #add the apnea to a list of apneas
+            i+=1 #move on to next peak
     else:
-        while i < len(extended_peak_view) - 1:
-            if extended_peak_view["Start"].iloc[i+1] - (extended_peak_view["Start"].iloc[i]+extended_peak_view["Width"].iloc[i]) > 0.7999:
-                apnea = Apnea("1/2", extended_peak_view["Start"].iloc[i] + extended_peak_view["Width"].iloc[i],[extended_peak_view["Start"].iloc[i+1] - (extended_peak_view["Start"].iloc[i] + extended_peak_view["Width"].iloc[i])])
+        while i < len(extended_peaks) - 2:
+            #same process but iterates from the sigh to 10 seconds after the sigh
+            if extended_peaks["Start"].iloc[i+1] - (extended_peaks["Start"].iloc[i]+extended_peaks["Width"].iloc[i]) > 0.7999:
+                apnea = Apnea("1/2", extended_peaks["Start"].iloc[i] + extended_peaks["Width"].iloc[i],[extended_peaks["Start"].iloc[i+1] - (extended_peaks["Start"].iloc[i] + extended_peaks["Width"].iloc[i])])
                 apneas.append(apnea)
             i+=1
     if len(apneas) < 1:
-        sigh.lack()
+        sigh.lack() #if there are no apneas after the sigh, the sigh may be a sniff, and needs to be highlighted differently
     return apneas
 
 def matching_apnea(start_time,apneas):
     return any(
-        apnea.start_time == start_time
+        apnea.start_time == start_time #if there's another apnea that has the same start time as the input, return True
         for apnea in apneas
     )
 
 def type3_apnea(peak_data, apneas):
     i = 0
-    while i < len(peak_data) - 1:
-        if peak_data["Start"].iloc[i+1] - (peak_data["Start"].iloc[i]+ peak_data["Width"].iloc[i]) > 0.7999 & (not matching_apnea(peak_data["Start"].iloc[i]+ peak_data["Width"].iloc[i],apneas)): 
+    while i < len(peak_data) - 2:
+        if peak_data["Start"].iloc[i+1] - (peak_data["Start"].iloc[i]+ peak_data["Width"].iloc[i]) > 0.7999 and (not matching_apnea(peak_data["Start"].iloc[i]+ peak_data["Width"].iloc[i],apneas)): 
+             #if there's a gap in the peaks big enough for an apnea AND it's not an apnea already saved, it's a type 3
              apnea = Apnea("3", peak_data["Start"].iloc[i]+peak_data["Width"][i], [peak_data["Start"].iloc[i+1], peak_data["Start"].iloc[i+1] - (peak_data["Start"][i] + peak_data["Width"][i])])
              apneas.append(apnea)
         i+=1
@@ -120,7 +125,8 @@ def apnea_combination(normalized_signal, apneas): #no idea if I wrote this corre
     removed_apneas = []
     for apnea in apneas:
         extended_view = peak_analysis(normalized_signal, apnea.start_time)
-        sigh_caught = find_sighs(extended_view,peak_means(extended_view))
+        ev_hmean, ev_amean = peak_means(extended_view)
+        sigh_caught = find_sighs(extended_view,ev_hmean,ev_amean)
        
         if len(sigh_caught) > 0:
             apnea.duration.append(apnea2.duration[0] for apnea2 in apneas if apnea.start_time < apnea2.start_time < sigh_caught[0].start_time)
@@ -129,5 +135,5 @@ def apnea_combination(normalized_signal, apneas): #no idea if I wrote this corre
             apnea.duration.append(apnea2.duration[0] for apnea2 in apneas if apnea.start_time < apnea2.duration[0] < extended_view["Time"].iloc[0])
             removed_apneas.append(apnea2 for apnea2 in apneas if apnea.start_time < apnea2.start_time < extended_view["Time"].iloc[0])
 
-    apneas = apneas - removed_apneas
+    apneas = [apnea for apnea in apneas if apnea not in removed_apneas]
     return apneas
--- PA_GUI.py
+++ PA_GUI.py
@@ -19,62 +19,63 @@ def plot():
 iter = 0
 window = Tk()
 
-class PA_IntroWindow:
+class PA_IntroWindow: #first window for taking the ascii file name
     def __init__(self,frame,state):
-        self.window = frame
-        self.window.title("FirstStep")
-        self.state = state
-        self.iter = iter
+        self.window = frame #takes the universal Tk
+        self.window.title("FirstStep") #names it after the section
+        self.state = state #takes the condition that defines the window shape
+        self.iter = iter #takes the current iteration based on the global
 
-        self.file_var = StringVar()
-        self.file_label = Label(self.window,text="Paste Filename Here:",font=('calibre',15,'bold'))
+        self.file_var = StringVar() #file name tkinter string
+        self.file_label = Label(self.window,text="Paste Filename Here:",font=('calibre',15,'bold')) #prompts the user for the filename
         self.file_label.grid(row=0,column=0)
-        self.file_entry = Entry(self.window, textvariable=self.file_var,font=('calibre',15,'normal'))
+        self.file_entry = Entry(self.window, textvariable=self.file_var,font=('calibre',15,'normal')) #allows the user to enter the filename
         self.file_entry.grid(row=0,column=1)
-        self.submit = Button(self.window, text="Submit", command=self.file_name)
+        self.submit = Button(self.window, text="Submit", command=self.file_name) #submits the contents of the textbox to a section that may activate the next window
         self.submit.grid(row=2,column=1)
-    def summon_window(self):
-        new = Analysis_Window(self.signal,self.sub_signal)
+    def summon_window(self): #summons the next window
+        new = Analysis_Window(self.signal,self.sub_signal) #initiates the next window
     def file_name(self):
-        e = ""
+        e = "" #to update error message
         try:
-            skiprows = pa.skiprows(self.iter)
-            signal, sub_signal = pa.signal_prep(self.file_var.get(),skiprows)
-            self.signal = signal
-            self.sub_signal = sub_signal
+            skiprows = pa.skiprows(self.iter) #take the rows that are needed to skip based on the iteration at the time
+            signal, sub_signal = pa.signal_prep(self.file_var.get(),skiprows) #acquire the 20 second and 10 second interval
+            self.signal = signal #signal is the 20 second interval
+            self.sub_signal = sub_signal #subsignal is the 10 second interval
 
-            error_note = Label(self.window,text=e,font=('calibre',15,'bold'))
+            error_note = Label(self.window,text=e,font=('calibre',15,'bold')) #removes the error note if there was a previous error
             error_note.grid(row=3,column=0)
 
-            file_acq = Label(self.window,text='File Acquired!',font=('calibre',15,'bold'))
+            file_acq = Label(self.window,text='File Acquired!',font=('calibre',15,'bold')) #alerts the user that the file name was functional
             file_acq.grid(row=3,column=0)
             
-            self.summon_window()
+            self.summon_window() #if so, go to next window
         except Exception as e:
             _, _, exc_tb = sys.exc_info()
             tb_details = traceback.extract_tb(exc_tb)
             filename, line_number, func_name, text = tb_details[-1]
-            print(f"The error is: {text}")
-            error_note = Label(self.window,text=e,font=('calibre',15,'bold'))
+            print(f"The error is: {text}") #to let me know what the error is while debugging
+            error_note = Label(self.window,text=e,font=('calibre',15,'bold')) #informs the user what type of error ocurred while trying to load the file
             error_note.grid(row=3,column=0)
 
 
 
 class Analysis_Window:
     def __init__(self, dataframe,sub_dataframe):
-        self.new_window(window)
-        self.width = self.window.winfo_width()
-        self.height = self.window.winfo_height()
-        self.iter = iter
+        self.new_window(window) #makes a new window based off of the master Tk window
+        self.width = self.window.winfo_width() #takes the width 
+        self.height = self.window.winfo_height() #and height of the current window for future reference
+        self.skiprows = pa.skiprows(iter)
 
-        self.frame_table = Frame(self.window, width=int(self.width/2), height=self.height)
+        self.frame_table = Frame(self.window, width=int(self.width/2), height=self.height) #frame for the 10 second datatable that takes up about a half of the screen
         self.main_data = dataframe
         self.subsection_data = sub_dataframe
 
-        self.event_frame = Frame(self.window, width=int(self.width/2), height=int(self.height/2))
-        self.display_data()
-        self.analyze_data()
-        self.display_events()
+        self.event_frame = Frame(self.window, width=int(self.width/2), height=int(self.height/2)) #frame for the list of all events
+        self.display_data() #displays the main data
+        self.analyze_data() #sends the 10 second interval through standard analysis
+        self.concatonate_data()
+        self.display_events() #display the list of events from the events dataframe
     def new_window(self,frame):
         self.window = Toplevel(master=frame)
         self.window.title("AnalysisWindow")
@@ -83,14 +84,14 @@ class Analysis_Window:
         self.frame_table.place(relx=0.0,rely=0.0,anchor="nw")
         self.table.show()
     def analyze_data(self):
-        self.peaks = pa.peak_analysis(self.subsection_data,self.iter)
+        self.peaks = pa.peak_analysis(self.subsection_data,(self.skiprows*(1/2000)))
         self.peaks_mean, self.peaks_area_mean = pa.peak_means(self.peaks)
         self.sighs = pa.find_sighs(self.peaks,self.peaks_mean,self.peaks_area_mean)
         self.apneas = []
         for sigh in self.sighs:
             self.apneas += pa.postsigh_apnea(self.main_data,sigh) 
         self.apneas += pa.type3_apnea(self.peaks,self.apneas)
-        self.apneas += pa.apnea_combination(self.main_data,self.apneas)
+        self.apneas = pa.apnea_combination(self.main_data,self.apneas)
     def concatonate_data(self):
         names = []
         starts = []
@@ -99,25 +100,24 @@ class Analysis_Window:
         question = []
         for sigh in self.sighs:
             names.append(sigh.name)
-            starts.append(sigh.start)
+            starts.append(sigh.start_time)
             durations.append(sigh.duration)
             types.append("N/A")
             question.append(sigh.questionable)
         for apnea in self.apneas:
             names.append(apnea.name)
-            starts.append(apnea.start)
+            starts.append(apnea.start_time)
             durations.append(apnea.duration)
             types.append(apnea.type)
             question.append("N/A")
         event_data = {"Event": names, "Start": starts, "Type": types, "Questionable": question}
-        self.event_frame = pd.Dataframe(data=event_data)
+        self.events_dataframe = pd.DataFrame(data=event_data)
     def display_events(self):
-        self.event_table = Table(self.frame_frame, dataframe=self.event_frame, showtoolbar=True, showstatusbar=True)
+        self.event_table = Table(self.event_frame, dataframe=self.events_dataframe, showtoolbar=True, showstatusbar=True)
         self.frame_table.place(relx=1.0,rely=1.0,anchor="se")
         self.table.show()
 
         
-
 
 IntroWindow = PA_IntroWindow(window,'zoomed')
 window.mainloop()
