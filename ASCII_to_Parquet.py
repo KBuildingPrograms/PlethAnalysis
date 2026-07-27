bd8797eed1d334369824281edf1682d60b36c63f
KBuildingPrograms <kyd.00583@gmail.com>
2026-07-27 14:23:01 -0500

    - ASCII to parquet converter, will be brought to main

--- ASCII_to_Parquet.py
+++ ASCII_to_Parquet.py
@@ -4,12 +4,14 @@ import pandas as pd
 csv_path = input("Paste path of ASCII here:")
 csv_path = csv_path.replace("\"","")
 print("Loading")
-csv_data = pd.read_csv(csv_path,sep="\\s+",index_col=False,names=["Time","Flow"],header=0,chunksize=100000)
+csv_data = pd.read_csv(csv_path,sep="\\s+",index_col=False,names=["Time","Flow"],header=0,chunksize=100000,dtype={"Time":"float32","Flow":"float32"})
 
 print("Generating Path")
 parquet_path = csv_path.replace(".ascii",".parquet")
 print("Moving to parquet")
+final_dataframe = pl.DataFrame(schema={"Time":pl.Float32,"Flow":pl.Float32})
 for chunk in csv_data:
-    polar_chunk = pl.from_pandas(chunk)
-    polar_chunk.write_parquet(parquet_path)
+   final_dataframe.extend(pl.from_pandas(chunk))
+print(final_dataframe)
+final_dataframe.write_parquet(parquet_path,compression="zstd",compression_level=3,statistics=False)
 print("Done!")
